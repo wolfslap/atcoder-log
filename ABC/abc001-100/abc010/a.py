3b2745bc09ff51c46ae7f6f96cd774a6e1aceda6
wolfslap <mukuro451@gmail.com>
2026-06-07 17:47:31 +0900

    abc010-aを解いた

--- ABC/abc001-100/abc010/a.py
+++ ABC/abc001-100/abc010/a.py
@@ -2,7 +2,12 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    pass
+    s = input().strip()
+    print(s+"pp")
 
 if __name__ == "__main__":
     main()
+
+# .strip()とは　空白や改行など目に見えないものを消すもの　基本は(s+"pp")でOK
+# import sys
+# input = sys.stdin.readline　の部分が勝手に改行してたっぽい？
