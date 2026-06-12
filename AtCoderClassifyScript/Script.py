6f0179c5cf7a2d1318a2fa1ff6a42955d269a281
wolfslap <mukuro451@gmail.com>
2026-06-12 20:37:20 +0900

    fix : #1 JSONファイルの情報と一致させるために名称変更

--- AtCoderClassifyScript/Script.py
+++ AtCoderClassifyScript/Script.py
@@ -9,3 +9,36 @@ def fetch_difficulty_data():
     except requests.RequestException as e:
         print(f"取得失敗: {e}")
         return None
+
+from pathlib import Path
+
+def get_problem_ids(directory: str) -> list[str]:
+    dir_path = Path(directory)
+    problem_ids = []
+    
+    for file in dir_path.iterdir():
+        if file.is_file():
+            problem_id = file.stem
+            problem_ids.append(problem_id)
+    
+    return problem_ids
+
+def get_color_folder(difficulty) -> str:
+    if difficulty is None:
+        return "unclassified"
+    if difficulty < 400:
+        return "gray"
+    elif difficulty < 800:
+        return "brown"
+    elif difficulty < 1200:
+        return "green"
+    elif difficulty < 1600:
+        return "cyan"
+    elif difficulty < 2400:
+        return "blue"
+    elif difficulty < 2800:
+        return "yellow"
+    elif difficulty < 3200:
+        return "orange"
+    else:
+        return "red"
